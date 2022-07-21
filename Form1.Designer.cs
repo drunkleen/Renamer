06a0b25cda6ca319f55a573eda04790d7ac66d82
Reza Darvishifar <reza.df.x@gmail.com>
2022-07-21 09:48:51 +0200

    source added

---
+++ Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace drag_and_drop
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.guna2ShadowForm1 = new Guna.UI2.WinForms.Guna2ShadowForm(this.components);
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ListBoxObjects = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.guna2Elipse2 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2Elipse3 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2Elipse4 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2ComboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.guna2TextDate = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.BorderRadius = 50;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // guna2ShadowForm1
+            // 
+            this.guna2ShadowForm1.BorderRadius = 50;
+            this.guna2ShadowForm1.TargetForm = this;
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 50;
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ListBoxObjects
+            // 
+            this.ListBoxObjects.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.ListBoxObjects.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ListBoxObjects.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ListBoxObjects.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListBoxObjects.ForeColor = System.Drawing.Color.White;
+            this.ListBoxObjects.FormattingEnabled = true;
+            this.ListBoxObjects.ItemHeight = 25;
+            this.ListBoxObjects.Location = new System.Drawing.Point(589, 79);
+            this.ListBoxObjects.Margin = new System.Windows.Forms.Padding(20);
+            this.ListBoxObjects.Name = "ListBoxObjects";
+            this.ListBoxObjects.Size = new System.Drawing.Size(252, 500);
+            this.ListBoxObjects.TabIndex = 3;
+            this.ListBoxObjects.SelectedIndexChanged += new System.EventHandler(this.ListBoxObjects_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Image = global::drag_and_drop.Properties.Resources.Untitled_12;
+            this.label1.Location = new System.Drawing.Point(27, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(539, 480);
+            this.label1.TabIndex = 2;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.DragDrop += new System.Windows.Forms.DragEventHandler(this.label1_DragDrop);
+            this.label1.DragOver += new System.Windows.Forms.DragEventHandler(this.label1_DragOver);
+            this.label1.DragLeave += new System.EventHandler(this.label1_DragLeave);
+            // 
+            // label2
+            // 
+            this.label2.AllowDrop = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 324);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(478, 54);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Drop The File to Rename";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.DragDrop += new System.Windows.Forms.DragEventHandler(this.label2_DragDrop);
+            this.label2.DragOver += new System.Windows.Forms.DragEventHandler(this.label2_DragOver);
+            // 
+            // guna2CircleButton1
+            // 
+            this.guna2CircleButton1.Animated = true;
+            this.guna2CircleButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.guna2CircleButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2CircleButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2CircleButton1.FillColor = System.Drawing.Color.Red;
+            this.guna2CircleButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CircleButton1.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButton1.Location = new System.Drawing.Point(837, 8);
+            this.guna2CircleButton1.Name = "guna2CircleButton1";
+            this.guna2CircleButton1.PressedColor = System.Drawing.Color.IndianRed;
+            this.guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButton1.Size = new System.Drawing.Size(21, 21);
+            this.guna2CircleButton1.TabIndex = 6;
+            this.guna2CircleButton1.Click += new System.EventHandler(this.guna2CircleButton1_Click);
+            // 
+            // guna2Elipse2
+            // 
+            this.guna2Elipse2.TargetControl = this.label1;
+            // 
+            // guna2Elipse3
+            // 
+            this.guna2Elipse3.TargetControl = this.ListBoxObjects;
+            // 
+            // guna2ComboBox1
+            // 
+            this.guna2ComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ComboBox1.BorderRadius = 15;
+            this.guna2ComboBox1.BorderThickness = 2;
+            this.guna2ComboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.guna2ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.guna2ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.guna2ComboBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.guna2ComboBox1.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.Font = new System.Drawing.Font("Nirmala UI", 14.25F);
+            this.guna2ComboBox1.ForeColor = System.Drawing.Color.White;
+            this.guna2ComboBox1.ItemHeight = 30;
+            this.guna2ComboBox1.Items.AddRange(new object[] {
+            ".pdf",
+            ".txt",
+            ".jpg",
+            ".png"});
+            this.guna2ComboBox1.Location = new System.Drawing.Point(692, 27);
+            this.guna2ComboBox1.Name = "guna2ComboBox1";
+            this.guna2ComboBox1.Size = new System.Drawing.Size(132, 36);
+            this.guna2ComboBox1.TabIndex = 8;
+            this.guna2ComboBox1.SelectedIndexChanged += new System.EventHandler(this.guna2ComboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 15.75F);
+            this.label3.Location = new System.Drawing.Point(577, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 30);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "File Type:";
+            // 
+            // guna2TextDate
+            // 
+            this.guna2TextDate.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(221)))), ((int)(((byte)(226)))));
+            this.guna2TextDate.BorderRadius = 16;
+            this.guna2TextDate.BorderThickness = 2;
+            this.guna2TextDate.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextDate.DefaultText = "";
+            this.guna2TextDate.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextDate.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextDate.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextDate.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextDate.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.guna2TextDate.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextDate.Font = new System.Drawing.Font("Nirmala UI", 14.25F);
+            this.guna2TextDate.ForeColor = System.Drawing.Color.White;
+            this.guna2TextDate.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextDate.Location = new System.Drawing.Point(99, 27);
+            this.guna2TextDate.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.guna2TextDate.Name = "guna2TextDate";
+            this.guna2TextDate.PasswordChar = '\0';
+            this.guna2TextDate.PlaceholderText = "";
+            this.guna2TextDate.SelectedText = "";
+            this.guna2TextDate.Size = new System.Drawing.Size(180, 36);
+            this.guna2TextDate.TabIndex = 10;
+            this.guna2TextDate.TextChanged += new System.EventHandler(this.guna2TextBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Nirmala UI", 15.75F);
+            this.label4.Location = new System.Drawing.Point(25, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 30);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Date:";
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.ClientSize = new System.Drawing.Size(870, 584);
+            this.Controls.Add(this.guna2TextDate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.guna2ComboBox1);
+            this.Controls.Add(this.guna2CircleButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ListBoxObjects);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = " ";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2ShadowForm guna2ShadowForm1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox ListBoxObjects;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse2;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse3;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse4;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextDate;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace drag_and_drop
+{
+    public partial class MainForm : Form
+    {
+
+
+        public static string varFileType;
+        public static string varFileAdresse;
+        public static string varFileNewName;
+        public static string varTodaysTime;
+        public static string varDateDDMMYYYY;
+        public static string varObjectName;
+
+
+        public void LoadUsers()
+        { 
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(@sPath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        string strListItem = sr.ReadLine();
+                        if (!String.IsNullOrEmpty(strListItem))
+                            ListBoxObjects.Items.Add(strListItem);
+                    }
+                }
+            }
+        }
+
+        //******** variables **********
+        const string sPath = "Data//ObjectList.txt";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            LoadUsers();
+
+        }
+
+        private void label1_DragOver(object sender, DragEventArgs e)
+        {
+
+            label2.ForeColor = Color.Red;
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void label1_DragDrop(object sender, DragEventArgs e)
+        {
+            label1.BackColor = Color.Transparent;
+
+            varFileType = guna2ComboBox1.SelectedItem.ToString();
+            if (varFileType == "")
+            {
+                varFileType = ".pdf";
+            }
+
+            string[] files = e.Data.GetData(DataFormats.FileDrop) as string[]; // get all files droppeds  
+
+            if (files != null && files.Any())
+            {
+                varFileAdresse = files.First(); //select the first one 
+
+                FileInfo file = new FileInfo(varFileAdresse); //rename file adresse
+                file.Rename(varObjectName + " " + guna2TextDate.Text + varFileType); //rename file new name
+            }
+            label2.ForeColor = Color.White;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            varDateDDMMYYYY = DateTime.Now.ToString("dd.MM.yyyy");
+            guna2TextDate.Text = varDateDDMMYYYY;
+            timer1.Enabled = false;
+
+        }
+
+        private void ListBoxObjects_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            varObjectName = ListBoxObjects.SelectedItem.ToString();
+        }
+
+        private void label1_DragLeave(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.Transparent;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_DragOver(object sender, DragEventArgs e)
+        {
+            label2.ForeColor = Color.Red;
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void label2_DragDrop(object sender, DragEventArgs e)
+        {
+
+
+
+            label1.BackColor = Color.Transparent;
+
+
+
+            varFileType = guna2ComboBox1.SelectedItem.ToString();
+            if (ListBoxObjects.SelectedItem == null)
+            {
+                varFileType = ".pdf";
+            }
+
+            string[] files = e.Data.GetData(DataFormats.FileDrop) as string[]; // get all files droppeds  
+            
+            if (files != null && files.Any())
+            {
+                varFileAdresse = files.First(); //select the first one 
+
+                FileInfo file = new FileInfo(varFileAdresse); //rename file adresse
+                file.Rename(varObjectName + " " + guna2TextDate.Text + varFileType); //rename file new name
+            }
+            label2.ForeColor = Color.White;
+        }
+
+        private void guna2CircleButton1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+namespace System.IO
+{
+    public static class ExtendedMethod
+    {
+        public static void Rename(this FileInfo fileInfo, string newName)
+        {
+            fileInfo.MoveTo(fileInfo.Directory.FullName + "\\" + newName);
+        }
+    }
+}
